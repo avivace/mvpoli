4ef137aed37467357bfe2faabd83246a4ae2d4f4
Antonio Vivace <puma95.95@gmail.com>
2016-11-23 20:57:41 +0100

    polyadd

--- tests.pl
+++ tests.pl
@@ -1,3 +1,32 @@
 -? as_polynomial(4 * xy ^ 2 + 9 *y^10, X),
 pprint_polynomial(X).
 
+
+poly(0)
+
+
+as_polynomial(7 * x ^ 3 * k^9 + k^9 + x^2 - 7 * x ^ 3 + 7 * x ^ 3, X),
+  pprint_polynomial(X).
+
+
+as_polynomial(0 * z - 0 * x ^ 2 + 1 * x, X),
+pprint_polynomial(X),
+polyplus(X,X, R),
+write("  STAMPO RISULTATO EHEHEHE XD  "),
+pprint_polynomial(R).
+
+
+
+as_polynomial(5 * x - 5 * x  + y, X),
+as_polynomial(z - y, Y),
+write("X:"),
+pprint_polynomials(X),
+write("   Y:"),
+print_polynomial(Y),
+polyminus(X, Y, Result),
+write("    X-Y:"),
+pprint_polynomial(Result).
+polyplus(X, Y, ResultS),
+write("    X+Y:"),
+pprint_polynomial(ResultS).
+
