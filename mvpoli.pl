4c66f5f42c00ca87d83f5ec039bd2f5ba54181ff
Antonio Vivace <puma95.95@gmail.com>
2016-11-28 15:33:16 +0100

    Documented the Parsing section

--- mvpoli.pl
+++ mvpoli.pl
@@ -1,15 +1,29 @@
 %%% Antonio Vivace 793509
 
-%% Parsing
-as_polynomial(X, poly(O)) :-
-  as_polynomial_p(X, P),
-  norm_p(P, O).
+% PARSING %
 
-as_monomial(X, NM) :-
-  as_monomial_p(X, C, Vd),
+%%%% as_polynomial/2
+%% True when poly(Monomials) unifies with the internal representation
+%% of the normalised polynomial.
+%
+as_polynomial(Input, poly(Monomials)) :-
+  as_polynomial_p(Input, RawMonomials),
+  norm_p(RawMonomials, Monomials).
+
+%%%% as_monomial/2
+%% True when Monomial unifies with the internal representation
+%% of the normalised monomial.
+%
+as_monomial(Input, Monomial) :-
+  as_monomial_p(Input, C, Vd),
   td(Vd, TD),
-  norm_m(m(C, TD, Vd), NM).
+  norm_m(m(C, TD, Vd), Monomial).
 
+%%%% as_polynomial/2
+%%%% as_polynomial(+Expression, -Monomials)
+%% True when Monomials unifies with the list of the Input
+%% monomials.
+%
 as_polynomial_p(X + Y, [M | Ms]) :-
   as_monomial(Y, M),
   !,
@@ -23,11 +37,20 @@ as_polynomial_p(X - Y, [M | Ms]) :-
 as_polynomial_p(X, [M]) :-
   as_monomial(X, M).
 
+
+%%%% as_monomial_i/2
+%%%% as_monomial_i(-Mono1, +Mono2)
+%% True when Mono1 unifies with the opposite of Mono2.
+%
 as_monomial_i(X, m(OC, TD, Vs)) :-
   as_monomial_p(X, C, Vs),
   OC is -C,
   td(Vs, TD).
 
+%%%% as_monomial/2
+%% True when Monomial unifies with the internal representation
+%% of the monomial.
+%
 as_monomial_p(X, X, []) :-
   number(X),
   !.
@@ -41,7 +64,11 @@ as_monomial_p(X, 1, [V]) :-
   asvar(X, V).
 
 % FIXME variables/atom question
-% asvar(_ ^ 0, []) :- !.
+%%%% asvar/2
+%%%% asvar(+Expression, -Var)
+%% True when Var unifies with v(N, X) where N is the exponent and
+%% X the atom representing the variable.
+%
 asvar(X ^ N, v(N, X)) :-
   integer(N),
   !,
@@ -54,6 +81,11 @@ asvar(X ^ N, v(N, X)) :-
 asvar(X, v(1, X)) :-
   atom(X).
 
+%%%% td/2
+%%%% td(+VarList, -N)
+%% True when N unifies with the sum of the grade of
+%% every variable in VarList
+%
 td([v(N1, _) | Vs], N) :-
   td(Vs, N2),
   !,
@@ -63,7 +95,14 @@ td([v(N1, _)], N1).
 
 td([], 0).
 
-%% Normalization
+% NORMALISATION %
+
+%%%% norm_m/2
+%%%% norm_m(+Mono1, -Mono2)
+%% True when Mono2 unifies with Mono1 normalised.
+%% The monomial normalisation consists in simplifying the monomial
+%% and reordering its variables in a lexicographical order.
+%
 norm_m(m(C, Td, []), m(C, Td, [])) :- !.
 norm_m(m(0, _, _), m(0, 0, [])) :- !.
 norm_m(m(C, 0, _), m(C, 0, [])) :- !.
@@ -72,6 +111,10 @@ norm_m(m(C, G, X), m(C, G, O)) :-
   sort(2, @=<, X, SX),
   norm_mm(SX, O).
 
+%%%% norm_mm/2
+%%%% norm_mm(+VarList1, -VarList2)
+%% True when VarList2 unifies with VarList1 simplified.
+%
 norm_mm([v(C1, X) , v(C2, X) | Ms], MMs) :-
   !,
   C3 is C1+C2,
@@ -84,11 +127,24 @@ norm_mm([v(C1, X), v(C2, Y) | Ms], [v(C1,X) | MMs]) :-
 
 norm_mm([v(C1, X)], [v(C1,X)]).
 
+%%%% norm_p/2
+%%%% norm_p(+Monomials, -NMonomials)
+%% True when NMonomials unifies with the list of normalised
+%% Monomials.
+%% Normalization consists in removing monomials with coefficient 0,
+%% simplifying similar monomials, and reordering them by descendant grade.
+%% When monomials have the same total degree, they should be discriminated
+%% by lexicographical order.
+%
 norm_p(Ms, OMs) :-
   sort(3, @=<, Ms, O),
   sort(2, @>=, O, Or),
   norm_pp(Or, OMs).
 
+%%%% norm_pp/2
+%%%% norm_pp(+Monomials, -SMonomials)
+%% True when SMonomials unifies with the list of simplyfied Monomials.
+%
 norm_pp([m(0, _, _), m(C2, S2, Y) | Ms], MMs) :-
   !,
   norm_pp([m(C2, S2, Y) | Ms], MMs).
@@ -103,25 +159,43 @@ norm_pp([m(C1, S1, X), m(C2, S2, Y) | Ms], [m(C1, S1, X) | MMs]) :-
   X \= Y,
   norm_pp([m(C2, S2, Y) | Ms], MMs).
 
-% norm_pp([m(0, _, _)], []) :- !.
 norm_pp([m(C1, S1, X)], [m(C1, S1, X)]).
 
-%% Checking
+% CHECKING %
+
+%%%% is_monomial/1
+%%%% is_monomial(Monomial)
+%% True when Monomial is a valid Monomial (internal representation).
+%
 is_monomial(m(_, TD, VPs)) :-
   integer(TD),
   TD >= 0,
   is_list(VPs).
 
+%%%% is_varpower/1
+%%%% is_varpower(Var)
+%% True when Var is a valid Variable (internal representation).
+%
 is_varpower(v(Power, VarSymbol)) :-
   integer(Power),
   Power >= 0,
   atom(VarSymbol).
 
+%%%% is_polynomial/1
+%%%% is_polynomial(Polynomial)
+%% True when Polynomial is a valid Polynomial (internal representation).
+%
 is_polynomial(poly(Monomials)) :-
   is_list(Monomials),
   foreach(member(M, Monomials), is_monomial(M)).
 
-%% Printing
+% PRINTING %
+
+%%%% pprint_polynomial/1
+%%%% pprint_polynomial(Poly)
+%% True when the Prolog interpreter succeds to print a human representation
+%% of Poly.
+%
 pprint_polynomial(poly(L)) :-
   pprint_pp(L).
 
@@ -173,7 +247,8 @@ pprint_v(v(X, Y)) :-
   write('^'),
   write(X).
 
-%% Operations
+% OPERATIONS %
+
 coefficients(poly(Ms), C) :-
   coefficients_l(Ms, C).
 coefficients_l([], []) :- !.
@@ -236,7 +311,7 @@ polyplus(poly(P), m(C, Td, Vars), Result) :-
   !,
   polyplus(poly(P), poly([m(C, Td, Vars)]), Result).
 
-% 0 is neutral for +
+% 0 is neutral for polyplus (and polyminus)
 polyplus(P1, poly([m(0, _, _)]), P1) :- !.
 polyplus(poly([m(0, _, _)]), P1, P1) :- !.
 
@@ -281,7 +356,7 @@ monotimes(m(C1, Td1, Vars1), m(C2, Td2, Vars2), m(C3, Td3, Vars3n)) :-
   norm_m(m(C3, Td3, Vars3), m(C3, Td3, Vars3n)).
 
 
-% polytimes accepts monomials too, as arguments.
+% Polytimes accepts monomials too, as arguments.
 polytimes(m(C, Td, Vars), m(C2, Td2, Vars), Result) :-
   !,
   polytimes(poly([m(C, Td, Vars)]), poly([m(C2, Td2, Vars)]), Result).
@@ -294,18 +369,19 @@ polytimes(poly(P), m(C, Td, Vars), Result) :-
   !,
   polytimes(poly(P), poly([m(C, Td, Vars)]), Result).
 
-% 0 is absorbing for *
+% 0 is absorbing for polytimes
 polytimes(_, poly([m(0, _, _)]), poly([m(0, 0, [])])) :- !.
 polytimes(poly([m(0,_, _)]), _, poly([m(0, 0, [])])) :- !.
 
-% 1 is neutral for *
+% 1 is neutral for polytimes
 polytimes(A, poly([m(1, _, [])]), A) :- !.
 polytimes(poly([m(1, _, [])]), A, A) :- !.
 
-polytimes(poly(M1s), poly(M2s), poly(Result)) :-
-  polytimes_m(M1s, M2s, Result).
+polytimes(poly(M1s), poly(M2s), poly(ResultF)) :-
+  polytimes_m(M1s, M2s, Result),
+  flatten(Result, ResultF).
 
-polytimes_m(M1s, [m(C, Td, Vars)], [R]) :-
+polytimes_m(M1s, [m(C, Td, Vars)], R) :-
   !,
   polymono(M1s, m(C, Td, Vars), R).
 
